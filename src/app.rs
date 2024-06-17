eba4f676163deeb2a630e64e13404d66ea2cf688
oddlama <oddlama@oddlama.org>
2024-06-17 17:52:45 +0200

    feat: finally consistent window width

--- src/app.rs
+++ src/app.rs
@@ -68,13 +68,14 @@ fn load_css() {
 }
 
 fn build_ui(app: &Application) {
-    let label = Label::builder()
-        .label("Initial: ...")
+    let main_box = gtk::Box::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .spacing(5)
         .can_target(false)
         .can_focus(false)
         .focus_on_click(false)
         .build();
-    label.add_css_class("live-text");
+    main_box.add_css_class("main-box");
 
     // Create a new window and present it
     let window = ApplicationWindow::builder()
@@ -85,17 +86,24 @@ fn build_ui(app: &Application) {
         .resizable(false)
         .can_target(false)
         .focusable(false)
-        .child(&label)
+        .default_width(1600) // Fixed when using layer-shell
+        .default_height(300) // Fixed when using layer-shell
+        .child(&main_box)
         .build();
 
     window.init_layer_shell();
     window.set_layer(Layer::Overlay);
     window.set_keyboard_mode(gtk_layer_shell::KeyboardMode::None);
+    window.set_anchor(gtk_layer_shell::Edge::Bottom, true);
+    window.set_margin(gtk_layer_shell::Edge::Bottom, 200);
     window.set_namespace("whisper-overlay");
 
     window.connect_realize(|window| {
         let wayland_surface = window.surface().and_downcast::<WaylandSurface>().unwrap();
         wayland_surface.set_input_region(&Region::create_rectangle(&RectangleInt::new(0, 0, 0, 0)));
+
+        // FIXME: disabled temporarily
+        // window.set_visible(false);
     });
 
     //window.set_monitor();
@@ -117,11 +125,30 @@ fn build_ui(app: &Application) {
 
     glib::spawn_future_local(async move {
         while let Ok(ui_action) = ui_receiver.recv().await {
-            println!("Got ui action {:#?}", ui_action);
             match ui_action {
-                UiAction::AddText(text) => label.set_text(&format!("{}{}", label.text(), text)),
-                UiAction::HideWindow => {}
-                UiAction::ShowWindow => {}
+                UiAction::AddText(text) => {} //label.set_text(&format!("{}{}", label.text(), text)),
+                UiAction::HideWindow => {
+                    //window.set_visible(false);
+                }
+                UiAction::ShowWindow => {
+                    let label = Label::builder()
+                        .wrap(true)
+                        .wrap_mode(gdk::pango::WrapMode::WordChar)
+                        .justify(gtk::Justification::Left)
+                        .halign(gtk::Align::Start)
+                        .can_target(false)
+                        .can_focus(false)
+                        .focus_on_click(false)
+                        .build();
+                    label.add_css_class("transcribed-text");
+                    label.set_markup(
+                        "<span color='red'>Hell o aegjro ijaoirgjoi jaroigja oirjgoairj oiajrgoiajrgoij oairgj aorgi rgi gir igrigri rgayrgairg iargi arigai rgargarg</span> <span color='green'>world</span>",
+                    );
+
+                    main_box.append(&label);
+
+                    //window.set_visible(true);
+                }
             }
         }
     });
